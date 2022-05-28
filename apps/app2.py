ed1c9003f3edf21fe4bf5c88effdf385328e1d48
frankchang1000 <frank.chang1000@gmail.com>
2022-05-28 13:04:53 -0400

    updated frontend

---
+++ apps/app2.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+from PIL import Image
+import tensorflow as tf
+import numpy as np
+from dictionary import *
+import pandas as pd
+
+
+def app():      
+
+    st.write("APP2")
+    st.image()
+    
